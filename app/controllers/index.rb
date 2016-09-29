7ccbbfab80e6e7f660e5bcd8ddd9fe91f8d61434
Jeff Fichtner & Kyle Dorsey <pair+jeff-fichtner.kylepdorsey@devbootcamp.com>
2016-09-29 17:51:44 -0500

    Change

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,14 +8,14 @@ post '/' do
 
   # text_input = "This is our response"
   uri = URI.parse("https://hooks.slack.com/services/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")
-  response = Net::HTTP::Post.new(uri)
-  response.content_type = "application/json"
-  response.body = JSON.dump({
-    "text" => q
+  app_response = Net::HTTP::Post.new(uri)
+  app_response.content_type = "application/json"
+  app_response.body = JSON.dump({
+    "text" => text_input
   })
 
-  resp = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
-    http.response(response)
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+    http.request(app_response)
   end
 end
 
