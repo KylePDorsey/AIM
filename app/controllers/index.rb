b94952c76367ad57f76369278398684237dd74cf
Jeff Fichtner & Kyle Dorsey <pair+jeff-fichtner.kylepdorsey@devbootcamp.com>
2016-09-29 17:44:49 -0500

    Change

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,14 +3,16 @@ require 'uri'
 require 'json'
 
 post '/' do
-  p request.body
+  return 401 unless request["token"] == ENV["SLACK_TOKEN"]
+  q = request["text"]
+
   # text_input = "This is our response"
-  # uri = URI.parse("https://hooks.slack.com/services/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")
-  # request = Net::HTTP::Post.new(uri)
-  # request.content_type = "application/json"
-  # request.body = JSON.dump({
-  #   "text" =>
-  # })
+  uri = URI.parse("https://hooks.slack.com/services/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")
+  response = Net::HTTP::Post.new(uri)
+  response.content_type = "application/json"
+  response.body = JSON.dump({
+    "text" => q
+  })
 
   # response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
   #   http.request(request)
