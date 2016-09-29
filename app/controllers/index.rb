c055601cc70a39cae229fc01f19dbcabafd2a3d9
Jeff Fichtner & Kyle Dorsey <pair+jeff-fichtner.kylepdorsey@devbootcamp.com>
2016-09-29 17:52:37 -0500

    Change

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,7 +6,7 @@ post '/' do
   # return 401 unless request["token"] == ENV["SLACK_TOKEN"]
   q = request["text"]
 
-  # text_input = "This is our response"
+  text_input = "This is our response"
   uri = URI.parse("https://hooks.slack.com/services/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")
   app_response = Net::HTTP::Post.new(uri)
   app_response.content_type = "application/json"
