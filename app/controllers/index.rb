2e1e1c4630d032ad728546d7145e9e7fb4eb7855
Jeff Fichtner & Kyle Dorsey <pair+jeff-fichtner.kylepdorsey@devbootcamp.com>
2016-09-29 18:13:31 -0500

    Getting fancy

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,14 +5,15 @@ require 'json'
 post '/' do
   # return 401 unless request["token"] == ENV["SLACK_TOKEN"]
   q = request["text"]
-  text_input = "This is our response"
-  return 200 if q == text_input
-  uri = URI.parse("https://hooks.slack.com/services/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")
-  app_response = Net::HTTP::Post.new(uri)
-  app_response.content_type = "application/json"
-  app_response.body = JSON.dump({
-    "text" => text_input
-  })
+  return 200 if request["user_name"] == "outgoing-webhook"
+  text_input = "Hello #{request["user_name"]}"
+  if q == "Hi"
+    uri = URI.parse("https://hooks.slack.com/services/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")
+    app_response = Net::HTTP::Post.new(uri)
+    app_response.content_type = "application/json"
+    app_response.body = JSON.dump({
+      "text" => text_input
+    })
 
   # response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
   #   http.request(app_response)
