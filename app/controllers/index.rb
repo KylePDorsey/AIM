9524f61153c8175aae2573ecb5c8267cdc39be39
Jeff Fichtner & Kyle Dorsey <pair+jeff-fichtner.kylepdorsey@devbootcamp.com>
2016-09-29 17:04:06 -0500

    Change route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,17 +4,17 @@ require 'json'
 
 post '/' do
   # p request.body
-  text_input = "This is our response"
-  uri = URI.parse("https://hooks.slack.com/services/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")
-  request = Net::HTTP::Post.new(uri)
-  request.content_type = "application/json"
-  request.body = JSON.dump({
-    "text" => request.text
-  })
+  # text_input = "This is our response"
+  # uri = URI.parse("https://hooks.slack.com/services/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")
+  # request = Net::HTTP::Post.new(uri)
+  # request.content_type = "application/json"
+  # request.body = JSON.dump({
+  #   "text" =>
+  # })
 
-  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
-    http.request(request)
-  end
+  # response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+  #   http.request(request)
+  # end
 end
 
 # p response.code
