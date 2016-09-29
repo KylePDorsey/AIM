938de4f9e0d33eb9e4a87fa577895df810698526
Jeff Fichtner & Kyle Dorsey <pair+jeff-fichtner.kylepdorsey@devbootcamp.com>
2016-09-29 16:45:24 -0500

    Change home route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,19 +2,19 @@ require 'net/http'
 require 'uri'
 require 'json'
 
+get '/' do
+  text_input = "This is our response"
+  uri = URI.parse("https://hooks.slack.com/services/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")
+  request = Net::HTTP::Post.new(uri)
+  request.content_type = "application/json"
+  request.body = JSON.dump({
+    "text" => text_input
+  })
 
-# text_input = "This is our response"
-# uri = URI.parse("https://hooks.slack.com/services/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")
-# request = Net::HTTP::Post.new(uri)
-# request.content_type = "application/json"
-# request.body = JSON.dump({
-#   "text" => text_input
-# })
-
-# response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
-#   http.request(request)
-# end
-
+  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+    http.request(request)
+  end
+end
 
 # p response.code
 # p response.body
@@ -22,7 +22,3 @@ require 'json'
 # post '/outgoing' do
 
 # end
-
-get '/' do
-  erb :form
-end
