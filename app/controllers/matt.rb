0e21684eeae3f79898baf0891bafa6aee86a5339
Jeff Fichtner <jff2501@gmail.com>
2016-09-29 22:41:47 -0500

    Loop error

--- app/controllers/matt.rb
+++ app/controllers/matt.rb
@@ -6,15 +6,15 @@ post '/matt' do
   # security token
   return 401 unless request["token"] == ENV["SLACK_TOKEN"]
   # it won't listen to itself
-  return 200 if request["user_name"] == "matt-baker"
+  return 204 if request["user_name"] == "matt-baker"
 
   message = request["text"] # yes, it's literally that simple
 
-  # if message.downcase.include? trigger
-  #   sleep(2)
-  #   return JSON.dump({
-  #     "text" => Faker::ChuckNorris.fact
-  #   })
-  # end
+  if message.downcase.include? trigger
+    sleep(2)
+    return JSON.dump({
+      "text" => Faker::ChuckNorris.fact
+    })
+  end
 
 end
