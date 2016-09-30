549a5736dbcf9dfd7d518f017229813facdb9624
Jeff Fichtner <jff2501@gmail.com>
2016-09-29 22:45:28 -0500

    Matt method

--- app/controllers/matt.rb
+++ app/controllers/matt.rb
@@ -6,15 +6,19 @@ post '/matt' do
   # security token
   return 401 unless request["token"] == ENV["SLACK_TOKEN"]
   # it won't listen to itself
-  return 204 if request["user_name"] == "matt-baker"
+  return 204 if matt
 
   message = request["text"] # yes, it's literally that simple
 
-  # if message.downcase.include? trigger
-  #   sleep(2)
-  #   return JSON.dump({
-  #     "text" => Faker::ChuckNorris.fact
-  #   })
-  # end
+  if message.downcase.include? trigger
+    sleep(2)
+    return JSON.dump({
+      "text" => Faker::ChuckNorris.fact
+    })
+  end
 
+end
+
+def matt
+  request["user_name"] == "matt-baker"
 end
