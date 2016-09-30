34bda2fce3483f2e716bbcd3bf4a0f73d73a7bd4
Jeff Fichtner <jff2501@gmail.com>
2016-09-29 22:38:07 -0500

    Final test

--- app/controllers/matt.rb
+++ app/controllers/matt.rb
@@ -1,23 +1,20 @@
-first_message_trigger = "chuck norris"
+trigger = "chuck norris"
 
-# home route
+# route
 post '/matt' do
 
   # security token
   return 401 unless request["token"] == ENV["SLACK_TOKEN"]
-  # bot won't listen to itself
+  # it won't listen to itself
   return 200 if request["user_name"] == "matt-baker"
 
-  message = request["text"]
-  if request["user_name"] == "jeff-fichtner"
+  message = request["text"] # yes, it's literally that simple
 
-    if message.downcase.include? first_message_trigger
-      sleep(3)
-      return JSON.dump({
-        "text" => Faker::ChuckNorris.fact
-      })
-    end
-
+  if message.downcase.include? trigger
+    sleep(2)
+    return JSON.dump({
+      "text" => Faker::ChuckNorris.fact
+    })
   end
 
 end
