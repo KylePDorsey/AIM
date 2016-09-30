280a7c28bfb08bd8836c964044643c82aa7f4864
Jeff Fichtner <jff2501@gmail.com>
2016-09-29 21:07:40 -0500

    First try at env var

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,6 @@
 # main route
 post '/' do
-  # return 401 unless request["token"] == ENV["SLACK_TOKEN"]
+  return 401 unless request["token"] == ENV["SLACK_TOKEN"]
   q = request["text"]
   return 200 if (request["user_name"] == "matt-baker" || request["user_name"] == "incoming-webhook")
   text_input = "You don't need to worry about that."
