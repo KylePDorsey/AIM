f16e550a26c4cb32feb6cf8ca0ad38dbd609b129
Jeff Fichtner <jff2501@gmail.com>
2016-09-29 20:54:05 -0500

    Change username logic gate

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,7 +5,7 @@ require 'json'
 post '/' do
   # return 401 unless request["token"] == ENV["SLACK_TOKEN"]
   q = request["text"]
-  return 200 if request["user_name"] == "matt-baker"
+  return 200 if (request["user_name"] == "matt-baker" || request["user_name"] == "incoming-webhook")
   text_input = "You don't need to worry about that."
   if q == "Can you explain this?"
     # uri = URI.parse("https://hooks.slack.com/services/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")
